06726e64cf0ccc8f821b4bcfa7720ec647684728
Chelsea Ash <crash1115@yahoo.com>
2026-08-31 16:15:21 -0400

    Add proficiency based mult settings

--- scripts/helpers/settings.js
+++ scripts/helpers/settings.js
@@ -1,18 +1,72 @@
 import { MODULE } from "../pf2e-downtime-scaling.js";
 
 export function registerSettings(){
-    game.settings.register(MODULE, "craftingMult", {
-        name: "Crafting Speed Multiplier",
-        hint: "When Crafting, the discount earned per day from the Earn Income table will be multiplied by this amount.",
+    game.settings.register(MODULE, "craftingMultTrained", {
+        name: "Crafting Speed Multiplier (Trained)",
+        hint: "When Trained in the skill used to craft, the discount earned per day from the Earn Income table will be multiplied by this amount.",
         scope: "world",
         config: true,
         default: "1",
         type: Number,
     });
 
-    game.settings.register(MODULE, "eiMult", {
-        name: "Earn Income Speed Multiplier",
-        hint: "When Earning Income, the amount earned per day from the Earn Income table will be multiplied by this amount. Does not stack with crafting multipliers.",
+    game.settings.register(MODULE, "craftingMultExpert", {
+        name: "Crafting Speed Multiplier (Expert)",
+        hint: "When Expert in the skill used to craft, the discount earned per day from the Earn Income table will be multiplied by this amount.",
+        scope: "world",
+        config: true,
+        default: "1",
+        type: Number,
+    });
+
+    game.settings.register(MODULE, "craftingMultMaster", {
+        name: "Crafting Speed Multiplier (Master)",
+        hint: "When Master in the skill used to craft, the discount earned per day from the Earn Income table will be multiplied by this amount.",
+        scope: "world",
+        config: true,
+        default: "1",
+        type: Number,
+    });
+
+    game.settings.register(MODULE, "craftingMultLegendary", {
+        name: "Crafting Speed Multiplier (Legendary)",
+        hint: "When Legendary in the skill used to craft, the discount earned per day from the Earn Income table will be multiplied by this amount.",
+        scope: "world",
+        config: true,
+        default: "1",
+        type: Number,
+    });
+
+    game.settings.register(MODULE, "eiMultTrained", {
+        name: "Earn Income Speed Multiplier (Trained)",
+        hint: "When Trained in the skill used to Earn Income, the amount earned per day from the Earn Income table will be multiplied by this amount. Does not stack with crafting multipliers.",
+        scope: "world",
+        config: true,
+        default: "1",
+        type: Number,
+    });
+
+    game.settings.register(MODULE, "eiMultExpert", {
+        name: "Earn Income Speed Multiplier (Expert)",
+        hint: "When Expert in the skill used to Earn Income, the amount earned per day from the Earn Income table will be multiplied by this amount. Does not stack with crafting multipliers.",
+        scope: "world",
+        config: true,
+        default: "1",
+        type: Number,
+    });
+
+    game.settings.register(MODULE, "eiMultMaster", {
+        name: "Earn Income Speed Multiplier (Master)",
+        hint: "When Master in the skill used to Earn Income, the amount earned per day from the Earn Income table will be multiplied by this amount. Does not stack with crafting multipliers.",
+        scope: "world",
+        config: true,
+        default: "1",
+        type: Number,
+    });
+
+    game.settings.register(MODULE, "eiMultLegendary", {
+        name: "Earn Income Speed Multiplier (Legendary)",
+        hint: "When Legendary in the skill used to Earn Income, the amount earned per day from the Earn Income table will be multiplied by this amount. Does not stack with crafting multipliers.",
         scope: "world",
         config: true,
         default: "1",
